5964d5a03fb22621be0b0bc3184cb9e81d7807e5
Ad Rienks <adrienks@adrienks.nl>
2019-12-14 18:33:36 +0100

    do not use arrow functions in this app!

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Fetch from "./Fetch.svelte";
+  import Form from "./Form.svelte";
+
+  let searchTerm;
+
+  function handleSubmit() {
+    // denk er om: geen arrow-functies gebruiken!
+    const { value } = this.elements.search;
+    searchTerm = value;
+  }
+</script>
+
+<Form {handleSubmit} />
+<Fetch {searchTerm} let:data>
+  <h1>A list of links</h1>
+  <ul>
+    {#each data as link}
+      <li>
+        <a href={link.url}>{link.title}</a>
+      </li>
+    {/each}
+  </ul>
+</Fetch>
